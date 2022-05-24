274c496d24b6867298cdad9179fdb3e6e2626a50
Mark Delgado <markdelgado.md@hotmail.com>
2022-05-24 11:25:38 -0400

    commit tree edcc1dd158dd5d04a80450c5cce561d1bef931bc

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction("name") {
+    return("Hi, my name is $(name).");
+}
+introduction();
+
+function introductionWithLanguage(name, language) {
+    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
+
+}introductionWithLanguage();
+
+
+    
